4f1f47e85b3f14aa0ebf920ac4a81f6e8f901bcc
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-10 00:48:44 -0300

    feature: User entity WIP criei uma entidade para ser o user do sistema

---
+++ app/src/main/java/edu/fatec/poo/model/User.java
@@ -0,0 +1,76 @@
+package edu.fatec.poo.model;
+
+public class User {
+    private long Id;
+    private String nome;
+    private String email;
+    private int telefone;
+    private String enderecoLogradouro;
+    private String enderecoCep;
+    private int enderecoNum;
+    private String complemento;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEnderecoLogradouro() {
+        return enderecoLogradouro;
+    }
+
+    public void setEnderecoLogradouro(String enderecoLogradouro) {
+        this.enderecoLogradouro = enderecoLogradouro;
+    }
+
+    public String getEnderecoCep() {
+        return enderecoCep;
+    }
+
+    public void setEnderecoCep(String enderecoCep) {
+        this.enderecoCep = enderecoCep;
+    }
+
+    public int getEnderecoNum() {
+        return enderecoNum;
+    }
+
+    public void setEnderecoNum(int enderecoNum) {
+        this.enderecoNum = enderecoNum;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
