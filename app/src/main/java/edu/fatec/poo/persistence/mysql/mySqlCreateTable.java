4e6468be11c63856827a33591f8c3d46036b5e27
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-10 10:04:56 -0300

    fix: skip if already exists

--- app/src/main/java/edu/fatec/poo/persistence/mysql/mySqlCreateTable.java
+++ app/src/main/java/edu/fatec/poo/persistence/mysql/mySqlCreateTable.java
@@ -30,7 +30,7 @@ public class mySqlCreateTable implements ICreateTable {
     @Override
     public void createTabelCliente() throws SQLException {
         String sql = """
-                CREATE TABLE cliente (
+                CREATE TABLE IF NOT EXISTS cliente (
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     nome VARCHAR(100) NOT NULL,
                     email VARCHAR(100) UNIQUE NOT NULL,
