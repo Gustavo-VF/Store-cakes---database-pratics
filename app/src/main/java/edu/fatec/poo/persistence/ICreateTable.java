14e5353b16b67537c7a3cd26b12a0a046a034251
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-10 08:48:27 -0300

    fix: ajustada a criação das tabelas

--- app/src/main/java/edu/fatec/poo/persistence/ICreateTable.java
+++ app/src/main/java/edu/fatec/poo/persistence/ICreateTable.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public interface ICreateTable {
     public boolean tableExists(String nomeTabela) throws SQLException;
 
-    public void createAll() throws SQLException;
+    public void createTableAll() throws SQLException;
 
-    public void createCliente() throws SQLException;
+    public void createTabelCliente() throws SQLException;
 }
