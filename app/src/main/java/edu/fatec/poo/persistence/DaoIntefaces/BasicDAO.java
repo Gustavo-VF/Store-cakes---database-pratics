90d973b3aa86240088e453d94db0841df8a0599b
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-22 23:04:41 -0300

    feature: created add to TipoProduto criei o add para o crudo do tipo produto

---
+++ app/src/main/java/edu/fatec/poo/persistence/DaoIntefaces/BasicDAO.java
@@ -0,0 +1,20 @@
+package edu.fatec.poo.persistence.DaoIntefaces;
+
+import edu.fatec.poo.model.TipoProduto;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BasicDAO<T> {
+    Optional<T> add(T object) throws SQLException, ClassNotFoundException;
+
+    Optional<T> findById(UUID id) throws SQLException, ClassNotFoundException;
+
+    Optional<List<T>> searchAll() throws SQLException, ClassNotFoundException;
+
+    Optional<T> update(T object) throws SQLException, ClassNotFoundException;
+
+    Optional<T> delete(T object) throws SQLException, ClassNotFoundException;
+}
--- app/src/main/java/edu/fatec/poo/persistence/sqlServer/DaoImplementations/TipoProdutoSqlImpl.java
+++ app/src/main/java/edu/fatec/poo/persistence/sqlServer/DaoImplementations/TipoProdutoSqlImpl.java
@@ -1,8 +1,15 @@
 package edu.fatec.poo.persistence.sqlServer.DaoImplementations;
 
+import edu.fatec.poo.model.TipoProduto;
 import edu.fatec.poo.persistence.DaoIntefaces.TipoProdutoDAO;
 import edu.fatec.poo.persistence.connection.ADaoConnector;
 import edu.fatec.poo.persistence.sqlServer.ConfiguredSqlConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 public class TipoProdutoSqlImpl implements TipoProdutoDAO {
 
@@ -11,5 +18,23 @@ public class TipoProdutoSqlImpl implements TipoProdutoDAO {
 
     TipoProdutoSqlImpl() {
         this.connector = new ConfiguredSqlConnector().getConector();
+    }
+
+    @Override
+    public Optional<TipoProduto> add(TipoProduto tipoProduto) throws SQLException, ClassNotFoundException {
+        if (tipoProduto == null || tipoProduto.getId() == null) {
+            return Optional.empty();
+        }
+
+        String sql = "INSERT INTO " + dbName + " (id, descricao) VALUES (?, ?);";
+        try (Connection c = connector.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql)) {
+
+            ps.setObject(1, tipoProduto.getId());
+            ps.setString(2, tipoProduto.getDescricao());
+
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected > 0 ? Optional.of(tipoProduto) : Optional.empty();
+        }
     }
 }
